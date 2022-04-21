a9881114448a3688ab8e91316ae54a8361c81111
garming <2180727350@qq.com>
2022-04-21 21:52:55 +0800

    fea

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -8,20 +8,14 @@ module.exports = [
             { text: 'vite 工程化', link: '/awesome/engineering/vite.md' },
             { text: 'webpack 工程化', link: '/awesome/engineering/webpack.md' },
             { text: 'rollup 工程化', link: '/awesome/engineering/rollup.md' },
+            
             { text: '代码质量', link: '/awesome/code-quality/' },
             { text: '面试', link: '/awesome/interview/' },
             { text: '性能优化', link: '/awesome/lighthouse/' },
             { text: '服务端渲染', link: '/awesome/ssr-render/' },
             { text: '前端架构', link: '/awesome/framework/' },
             { text: 'Typescript', link: '/awesome/typescript/' },
-            { text: '知识库', link: '/awesome/knowledge-base/' },
-            { 
-                text: 'package', 
-                link: '/awesome/npm/',
-                items: [
-                    { text: 'big.js', link: '/awesome/npm/big.js.md' },
-                ]
-            }
+            { text: '知识库', link: '/awesome/knowledge-base/' }
         ]
     },
     { text: '时间线', link: '/timeline/', icon: 'reco-date' },
@@ -33,5 +27,12 @@ module.exports = [
             { text: '讯康检中系统分科管理', link: '/bug/bug-1/' },
         ] 
     },
-    { text: '随笔', link: '/article/', icon: 'reco-blog' }
+    { 
+        text: 'npm', 
+        link: '/npm/', 
+        icon: 'reco-npm',
+        items: [
+            { text: 'big.js', link: '/npm/big.js.md' },
+        ]
+    }
 ]
