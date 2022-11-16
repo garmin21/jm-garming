eb2ea8e3bdfa47cf1571ce43652a7ac04e6a5916
garming <2180727350@qq.com>
2022-11-16 22:22:01 +0800

    feat: babel

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,47 +1,48 @@
 module.exports = [
-    { 
-        text: '文章', 
-        ariaLabel: 'article Menu', 
-        icon: 'reco-coding', 
-        items: [
-            { text: 'web 摄像头', link: '/article/01.md' },
-            { text: '大数据量加载处理', link: '/article/02.md' },
-            { text: '判断对象是否具有属性的 5 种方法', link: '/article/03.md' },
-            { text: '远程组件的研究分析', link: '/article/04.md' },
-            { text: '使用babel 操作 js 文件', link: '/article/05.md' },
-            { text: '自定义一个websocket工具函数', link: '/article/06.md' },
-        ]
-    },
-    { text: 'Vue3', link: '/awesome/engineering/vue3.md', icon: 'reco-other' },
-    { 
-        text: '收藏', 
-        ariaLabel: 'awesome Menu',
-        icon: 'reco-suggestion',
-        items:[
-            { text: 'vite 工程化', link: '/awesome/engineering/vite.md' },
-            { text: 'webpack 工程化', link: '/awesome/engineering/webpack.md' },
-            { text: 'rollup 工程化', link: '/awesome/engineering/rollup.md' },
-            { text: '代码质量', link: '/awesome/code-quality/' },
-            { text: '面试', link: '/awesome/interview/' },
-            { text: '性能优化', link: '/awesome/lighthouse/' },
-            { text: '服务端渲染', link: '/awesome/ssr-render/' },
-            { text: '前端架构', link: '/awesome/framework/' },
-            { text: 'Typescript', link: '/awesome/typescript/' },
-            { text: '知识库', link: '/awesome/knowledge-base/' }
-        ]
-    },
-    // { text: '时间线', link: '/timeline/', icon: 'reco-date' },
-    { 
-        text: 'npm', 
-        link: '/npm/', 
-        icon: 'reco-npm',
-        items: [
-            { text: 'big.js', link: '/npm/big.js.md' },
-            { text: 'file-saver', link: '/npm/file-saver.md' },
-            { text: 'async-validator', link: '/npm/async-validator.md' },
-            { text: 'docx-preview', link: '/npm/docx-preview.md' },
-            { text: '@vueuse/core', link: '/npm/@vueuse.md' },
-            { text: 'jest', link: '/npm/jest.md' },
-        ]
-    }
-]
+  {
+    text: "文章",
+    ariaLabel: "article Menu",
+    icon: "reco-coding",
+    items: [
+      { text: "web 摄像头", link: "/article/01.md" },
+      { text: "大数据量加载处理", link: "/article/02.md" },
+      { text: "判断对象是否具有属性的 5 种方法", link: "/article/03.md" },
+      { text: "远程组件的研究分析", link: "/article/04.md" },
+      { text: "使用babel 操作 js 文件", link: "/article/05.md" },
+      { text: "自定义一个websocket工具函数", link: "/article/06.md" },
+    ],
+  },
+  { text: "Vue3", link: "/awesome/engineering/vue3.md", icon: "reco-other" },
+  {
+    text: "收藏",
+    ariaLabel: "awesome Menu",
+    icon: "reco-suggestion",
+    items: [
+      { text: "vite 工程化", link: "/awesome/engineering/vite.md" },
+      { text: "webpack 工程化", link: "/awesome/engineering/webpack.md" },
+      { text: "rollup 工程化", link: "/awesome/engineering/rollup.md" },
+      { text: "代码质量", link: "/awesome/code-quality/" },
+      { text: "面试", link: "/awesome/interview/" },
+      { text: "性能优化", link: "/awesome/lighthouse/" },
+      { text: "服务端渲染", link: "/awesome/ssr-render/" },
+      { text: "前端架构", link: "/awesome/framework/" },
+      { text: "Typescript", link: "/awesome/typescript/" },
+      { text: "知识库", link: "/awesome/knowledge-base/" },
+    ],
+  },
+  // { text: '时间线', link: '/timeline/', icon: 'reco-date' },
+  {
+    text: "npm",
+    link: "/npm/",
+    icon: "reco-npm",
+    items: [
+      { text: "big.js", link: "/npm/big.js.md" },
+      { text: "file-saver", link: "/npm/file-saver.md" },
+      { text: "async-validator", link: "/npm/async-validator.md" },
+      { text: "docx-preview", link: "/npm/docx-preview.md" },
+      { text: "@vueuse/core", link: "/npm/@vueuse.md" },
+      { text: "jest", link: "/npm/jest.md" },
+      { text: "babel", link: "/npm/babel.md" },
+    ],
+  },
+];
---
+++ docs/npm/babel.md
@@ -0,0 +1,24 @@
+---
+title: babel
+date: 2022-11-16
+tags:
+  - npm
+---
+
+<img :src="$withBase('/WechatIMG70.jpeg')" width="width:100%" height="100%" />
+
+语法的转化，是需要配置插件进行转换的
+
+<img :src="$withBase('/WechatIMG71.jpeg')" width="width:100%" height="100%" />
+
+总结：
+babel 是一个语法转化器，用于将一些高级的语法转化为 低级的语法，以至于让浏览器识别。
+
+1. `@babel/core` babel 的核心包，负责 编译的工作
+2. `@babel/cli` babel 的命令行执行工具
+3. `@babel/preset-env` babel 的预设包，已经包含了所有语法转换的插件了
+
+### babel 插件
+
+1. `@babel/plugin-transform-arrow-functions` 转化箭头函数
+2. ....等插件
